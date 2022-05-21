c9b256bff8f3cb600b3964cc8e07c76a58e80d15
Rustem <ryuzikc@gmail.com>
2022-05-21 15:48:12 +0300

    swiper slider

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,98 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <swiper
+        :options="swiperOptions"
+        :slidesPerView="1"
+        :spaceBetween="30"
+        :breakpoints="swiperBreakPoints"
+        :pagination="{
+      clickable: true,
+    }"
+        :navigation="true"
+        :modules="modules"
+        class="mySwiper"
+    >
+      <swiper-slide v-for="(slide, index) in slides">
+        <img :src="slide.url" alt="">
+      </swiper-slide>
+    </swiper>
+    <div class="custom-buttons">
+      <button @click class="btn prev-btn">Назад</button>
+      <button class="btn next-btn">Вперед</button>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
+import { Swiper, SwiperSlide } from "swiper/vue/swiper-vue";
 
+// Import Swiper styles
+import "swiper/swiper-bundle.css";
+
+import {Pagination} from "swiper";
+import {Navigation} from 'swiper'
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    HelloWorld,
+    Swiper,
+    SwiperSlide,
+  },
+  setup() {
+    return {
+      modules: [Pagination,Navigation],
+    };
+  },
+  data(){
+    return {
+      slides: [],
+      swiperBreakPoints: {
+        '640': {
+          slidesPerView: 2,
+          spaceBetween: 20,
+        },
+        '768': {
+          slidesPerView: 4,
+          spaceBetween: 40,
+        },
+        '1024': {
+          slidesPerView: 5,
+          spaceBetween: 50,
+        },
+      }
+    }
+  },
+  async created(){
+    this.slides = await (await this.axios.get('https://jsonplaceholder.typicode.com/photos?_limit=20')).data
   }
 }
 </script>
+<style lang="scss">
+.container{
+  max-width: 1200px;
+  margin: 0 auto;
+}
+.swiper-slide{
+  width: 300px;
+  img{
+    width: 100%;
+  }
+}
+.custom-buttons{
+  display: flex;
+  column-gap: 15px;
+  margin-top: 30px;
+}
+.btn{
+  background-color: #42b983;
+  color: #fff;
+  font-size: 18px;
+  font-family: Arial,serif;
+  font-weight: 500;
+  border: none;
+  padding: 10px 30px;
+  border-radius: 10px;
+}
+</style>
